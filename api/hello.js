3cda7c17a7550275f90cca665d945e61314db3ae
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-21 22:57:36 -0500

    Añadir funcion lambda de prueba

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,34 @@
-module.exports = (req, res) => {
+import chromium from "chrome-aws-lambda";
+
+export default async (req, res) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath:
+        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto("https://www.npmjs.com/package/chrome-aws-lambda");
+
+    result = await page.title();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
   res.json({
     hola: "mundo",
+    resp: result,
   });
 };
