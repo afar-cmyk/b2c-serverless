eac0a8ff0c046478d02f6adc402e41d4addd9f09
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-22 09:40:45 -0500

    Añadir las funciones cop y btc

---
+++ api/cop.js
@@ -0,0 +1,29 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+export default async (req, res) =>{
+
+  let xpath = '//*[@id="quote-page-strip"]/div[3]/div/div[2]/span[1]';
+
+  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  } : {
+    args: [],
+    executablePath: process.env.CHROME_EXECUTABLE_PATH
+  });
+
+
+  const page = await browser.newPage();
+
+  await page.goto("https://www.cnbc.com/quotes/COP=");
+
+  const [cop] = await page.$x(xpath);
+  const copContenido = await cop.getProperty("textContent");
+  const copCrudo = await copContenido.jsonValue();
+
+  res.json({
+    copCrudo,
+  });
+}
