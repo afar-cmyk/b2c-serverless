e3109f639271d46a6c8dfd1ba6bee7474ff919dc
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-22 10:30:03 -0500

    Predeterminar las rutas rapidas como funciones principales

--- api/btc.js
+++ api/btc.js
@@ -1,23 +1,26 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
-export default async (req, res) =>{
+export default async (req, res) => {
+  let xpath =
+    '//*[@id="__next"]/div[1]/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div/span';
 
-  let xpath = '//*[@id="quote-page-strip"]/div[3]/div/div[2]/span[1]';
-
-  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  } : {
-    args: [],
-    executablePath: process.env.CHROME_EXECUTABLE_PATH
-  });
-
+  const browser = await puppeteer.launch(
+    process.env.AWS_EXECUTION_ENV
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath: process.env.CHROME_EXECUTABLE_PATH,
+        }
+  );
 
   const page = await browser.newPage();
 
-  await page.goto("https://www.cnbc.com/quotes/BTC.CB=");
+  await page.goto("https://coinmarketcap.com/currencies/bitcoin/");
 
   const [btc] = await page.$x(xpath);
   const btcContenido = await btc.getProperty("textContent");
@@ -28,4 +31,4 @@ export default async (req, res) =>{
   res.json({
     btcCrudo,
   });
-}
+};
--- api/cop.js
+++ api/cop.js
@@ -1,26 +1,28 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
-export default async (req, res) =>{
+export default async (req, res) => {
+  let xpath = '//input[@id="answer"]';
 
-  let xpath = '//*[@id="quote-page-strip"]/div[3]/div/div[2]/span[1]';
-
-  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  } : {
-    args: [],
-    executablePath: process.env.CHROME_EXECUTABLE_PATH
-  });
-
+  const browser = await puppeteer.launch(
+    process.env.AWS_EXECUTION_ENV
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath: process.env.CHROME_EXECUTABLE_PATH,
+        }
+  );
 
   const page = await browser.newPage();
 
-  await page.goto("https://www.cnbc.com/quotes/COP=");
+  await page.goto("https://www.currency.me.uk/convert/usd/cop");
 
   const [cop] = await page.$x(xpath);
-  const copContenido = await cop.getProperty("textContent");
+  const copContenido = await cop.getProperty("value");
   const copCrudo = await copContenido.jsonValue();
 
   await browser.close();
@@ -28,4 +30,4 @@ export default async (req, res) =>{
   res.json({
     copCrudo,
   });
-}
+};
